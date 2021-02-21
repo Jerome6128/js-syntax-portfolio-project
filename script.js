5417fa10bc1ea2a846ef29453ec3afa8b288b83e
Jerome Gratelle <jerome.gratelle@air-indemnite.com>
2021-02-21 17:53:24 +0100

    store message components

---
+++ script.js
@@ -0,0 +1,9 @@
+//Define a list of artists
+const artists = ['Jean-Jacques Goldman', 'Christophe Colomb', 'Leonard da Vinci', 'Jesus', 'Marie Curie', 'Hugh Hefner'];
+
+//Define a list of actions
+const actions = ['has written', 'has sailed to', 'has painted', 'has met', 'has discovered', 'has pusblished'];
+
+//Define a list of objects
+const objects = ['Bonne idée', 'America', 'Mona Lisa', 'God', 'radioactivity', 'playboy'];
+
